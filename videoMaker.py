23a8d8647808bfcfe475950537b78c27e70bda30
J4ckTheSn4ck <jacksonbenoitlong@gmail.com>
2024-02-26 01:34:17 -0600

    Added tts capabilities

--- videoMaker.py
+++ videoMaker.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from gtts import gTTS
+from gtts.tokenizer import pre_processors
+import gtts.tokenizer.symbols
+import re
+
+gtts.tokenizer.symbols.SUB_PAIRS.append(
+    ('AITA', 'am i the asshole')
+)
+
+def remove_emojis(text):
+    # Define a regular expression pattern to match emojis
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+
+    # Remove emojis from the text
+    return emoji_pattern.sub(r'', text)
 
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless")  # Optional: Run Chrome in headless mode
@@ -15,7 +35,7 @@ time.sleep(1)
 
 posts = driver.find_elements(By.CSS_SELECTOR, '.w-full.m-0')
 num = 2
-postnum = 5
+postnum = 2
 
 title = posts[postnum].get_attribute('aria-label')
 link = posts[postnum].find_elements(By.XPATH, '*')[0].find_elements(By.XPATH, '*')[0].get_attribute('href')
@@ -33,6 +53,8 @@ while True:
         num += 1
     except:
         break
+text = remove_emojis(text)
 print(text)
 
-input()
+tts = gTTS(text)
+tts.save('text.mp3')
